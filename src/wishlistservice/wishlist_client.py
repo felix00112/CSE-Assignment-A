c94c74d0c41746458a58d97fde41ece0023c128f
paul <paul.pohlitz@gmx.de>
2024-12-18 15:18:50 +0100

    wishlists overview and adding wishlists works

---
+++ src/wishlistservice/wishlist_client.py
@@ -0,0 +1,9 @@
+import grpc
+import demo_pb2_grpc
+
+from logger import getJSONLogger
+logger = getJSONLogger('wishlistservice-server')
+
+if __name__ == "__main__":
+    channel = grpc.insecure_channel('[::]:50052')
+    stub = demo_pb2_grpc.WishlistServiceStub(channel)
