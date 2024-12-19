18286f1836f85b2446fba76b49fa68f12d0554f1
Paul <paul.pohlitz@gmx.de>
2024-12-19 17:43:59 +0100

    Delete src/wishlistservice/wishlist_client.py

--- src/wishlistservice/wishlist_client.py
+++
@@ -1,9 +0,0 @@
-import grpc
-import demo_pb2_grpc
-
-from logger import getJSONLogger
-logger = getJSONLogger('wishlistservice-server')
-
-if __name__ == "__main__":
-    channel = grpc.insecure_channel('[::]:50052')
-    stub = demo_pb2_grpc.WishlistServiceStub(channel)
