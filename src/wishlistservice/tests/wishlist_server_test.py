123730cd05da97573e9c9756e20a5a8b508c12e7
Felix <felix-hauptmann@gmx.de>
2024-12-19 15:28:04 +0100

    Add unit tests for wishlist server helper methods
    Introduced comprehensive unit tests for `get_redis_key` and `get_wishlist_items` methods to ensure proper functionality. Updated and expanded mock usage for Redis operations and enhanced test coverage in wishlist server logic.

--- src/wishlistservice/tests/wishlist_server_test.py
+++ src/wishlistservice/tests/wishlist_server_test.py
@@ -1,8 +1,9 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pytest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, MagicMock, call, create_autospec
 import demo_pb2
 from wishlist_server import WishlistService
 
@@ -13,18 +14,61 @@ def wishlist_server():
     server.redisInstance = Mock()
     return server
 
+
 @pytest.fixture
 def mock_request():
     request = Mock()
-    request.user_id = "1"
     return request
+
 
 @pytest.fixture
 def mock_context():
-    return Mock()
+    return MagicMock()
 
+
+# Unit Test for helper methods
+
+
+def test_get_redis_key(wishlist_server):
+    # Arrange
+    request = MagicMock()
+    request.user_id = "1"
+    request.name = "books"
+
+    # Act
+    result = wishlist_server.get_redis_key(request)
+
+    # Assert
+    assert result == "1:books"
+
+
+def test_get_wishlist_items(wishlist_server, mock_context):
+    # Arrange
+    wishlistKey = "user:1:wishlist:books"
+
+    # mock for redids (contains multiple products + "empty")
+    wishlist_server.redisInstance.smembers = MagicMock(
+        return_value={"item1", "item2", "empty"}
+    )
+
+    # Act
+    response = wishlist_server.get_wishlist_items(wishlistKey, mock_context)
+
+    # Assert
+    # Ensure that smembers is called with correct key
+    wishlist_server.redisInstance.smembers.assert_called_once_with(wishlistKey)
 
 def test_getAllWishlists(wishlist_server, mock_request, mock_context):
+    expected_items = {"item1", "item2"}
+
+    returned_items = {item.product_id for item in response}
+
+    # Check that "empty" is filtered and response-items are in the correct format
+    assert len(response) == 2
+    assert all(isinstance(item, demo_pb2.WishlistItem) for item in response)
+    assert returned_items == expected_items
+
+
     # Arrange
     redis_keys = ["1:books", "1:electronics"]
     wishlist_items1 = [demo_pb2.WishlistItem(product_id="1")]
